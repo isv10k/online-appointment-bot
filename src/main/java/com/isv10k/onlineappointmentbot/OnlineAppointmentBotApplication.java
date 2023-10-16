e0747e6d85b7cb107c535b9fbb8bde4e27daada1
Sergey Ilchenko <isv10k@gmail.com>
2023-10-16 12:00:08 +0500

    initial commit

---
+++ src/main/java/com/isv10k/onlineappointmentbot/OnlineAppointmentBotApplication.java
@@ -0,0 +1,13 @@
+package com.isv10k.onlineappointmentbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAppointmentBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineAppointmentBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isv10k/onlineappointmentbot/OnlineAppointmentBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isv10k.onlineappointmentbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAppointmentBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
